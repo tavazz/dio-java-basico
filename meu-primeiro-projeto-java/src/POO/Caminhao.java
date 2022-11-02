1f0945701f16bbd2ef32ce6c5d0c58cc42c01d4b
tavazz <gabriel.tavas10@gmail.com>
2022-11-02 18:25:14 -0300

    aprendendo-poo

---
+++ meu-primeiro-projeto-java/src/POO/Caminhao.java
@@ -0,0 +1,5 @@
+package POO;
+
+public class Caminhao extends Veiculo {
+    
+}
---
+++ meu-primeiro-projeto-java/src/POO/Main.java
@@ -0,0 +1,52 @@
+package POO;
+import java.util.Scanner;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        
+        double valorGasolina = 5.85d;
+
+
+
+        Scanner scan = new Scanner(System.in);
+
+        Carro carro1 = new Carro();
+
+        System.out.print("Cor: ");
+        
+        carro1.setCor(scan.nextLine());
+
+        System.out.println("Modelo:");
+        
+        carro1.setModelo(scan.nextLine());
+
+        System.out.println("Qtd Tanque: ");
+        carro1.setCapacidadeTanque(scan.nextInt());
+
+
+        System.out.println("A cor é: " + carro1.getCor());
+        System.out.println("O Modelo é: " + carro1.getModelo());
+        System.out.println("Capacidade do tanque: " + carro1.getCapacidadeTanque());
+
+        
+
+
+
+        System.out.println("Valor de tanque cheio: " + carro1.totalTanque(valorGasolina));
+
+
+
+
+        
+        
+
+
+
+    }
+    
+}
