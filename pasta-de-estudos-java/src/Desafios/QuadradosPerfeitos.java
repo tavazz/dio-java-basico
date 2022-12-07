ec50f89c90d84c98df4376815c9fd8bdd2bd5067
tavazz <gabriel.tavas10@gmail.com>
2022-12-06 22:35:21 -0300

    adicionando-desafios

---
+++ pasta-de-estudos-java/src/Desafios/QuadradosPerfeitos.java
@@ -0,0 +1,42 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class QuadradosPerfeitos {
+
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[] d = new int[n + 1];
+
+
+        for(int i = 1; i < n + 1; i++){
+            d[i] = i;
+        }
+
+        for (int i : d) {
+
+            int sqrt = Math.sqrt(int )
+            
+        }
+
+        
+
+
+                       
+
+        
+        
+
+
+
+
+    }
+
+
+    
+}
